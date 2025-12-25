948f90147c80e794d00ea5d5b9f165553d24c10a
Athmik-009 <athmiksooda9@gmail.com>
2025-12-25 13:26:45 +0530

    edit and delete

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app=express();
 const path=require('path');
 const mongoose=require('mongoose');
 const Campground=require('./models/campground');
+const methodOverride=require('method-override');
 
 async function connectDB() {
   try {
@@ -19,6 +20,7 @@ connectDB();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded({ extended: true }));//to read form data
+app.use(methodOverride('_method'));//to support PUT and DELETE requests
 
 
 app.get('/',(req,res)=>{
@@ -41,7 +43,15 @@ app.get('/campgrounds/:id',async(req,res)=>{
     const campground=await Campground.findById(req.params.id);
     res.render('campgrounds/show',{ campground });
 });
-
+app.get('/campgrounds/:id/edit',async(req,res)=>{
+    const campground=await Campground.findById(req.params.id);
+    res.render('campgrounds/edit',{ campground });
+});
+app.put('/campgrounds/:id',async(req,res)=>{
+    const { id } = req.params;
+    await Campground.findByIdAndUpdate(id, req.body.campground);
+    res.redirect(`/campgrounds/${id}`);
+});
 app.get('/makecampground',async(req,res)=>{
     const camp=new Campground({
         title:"My Campground",
@@ -51,6 +61,11 @@ app.get('/makecampground',async(req,res)=>{
     });
     await camp.save();
     res.send("Campground created");
+});
+app.delete('/campgrounds/:id',async(req,res)=>{
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
 });
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
