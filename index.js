3b2106b065383a9d34c481bf72db036f2820b20a
Athmik-009 <athmiksooda9@gmail.com>
2025-12-25 13:05:35 +0530

    create campground

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ connectDB();
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded({ extended: true }));//to read form data
+
 
 app.get('/',(req,res)=>{
     res.render('home');
@@ -26,6 +28,14 @@ app.get('/',(req,res)=>{
 app.get('/campgrounds',async(req,res)=>{
     const campgrounds=await Campground.find({});
     res.render('campgrounds/index',{ campgrounds });
+});
+app.get('/campgrounds/new',(req,res)=>{
+    res.render('campgrounds/new');
+});
+app.post('/campgrounds',async(req,res)=>{
+    const campground=new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 });
 app.get('/campgrounds/:id',async(req,res)=>{
     const campground=await Campground.findById(req.params.id);
