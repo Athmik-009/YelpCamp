2e205fbd291e9a29063d830cd30920207e77b087
Athmik-009 <athmiksooda9@gmail.com>
2026-01-02 15:53:55 +0530

    defining controllers

---
+++ controllers/campgrounds.js
@@ -0,0 +1,63 @@
+const Campground = require('../models/campground');
+
+module.exports.index = async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
+}
+
+module.exports.renderNewForm = (req,res)=>{
+    // if(!req.isAuthenticated()){//isAuthenticated method is added by passport to req object to check if user is logged in
+    //     req.flash('error','You must be signed in first!');
+    //     return res.redirect('/login');
+    // }//moving this to a middleware
+    res.render('campgrounds/new');
+};
+
+module.exports.createCampground = async(req,res)=>{//we are adding isLoggedIn middleware to protect this route from postman attacks
+    const campground=new Campground(req.body.campground);
+    campground.author=req.user._id;//set the author of the campground to the currently logged in user
+    await campground.save();
+    req.flash('success','Successfully made a new campground!');
+    res.redirect(`/campgrounds/${campground._id}`);
+};
+
+module.exports.showCampground =async(req,res)=>{
+    const campground=await Campground.findById(req.params.id).populate({//populate the reviews with all their authors
+        path:'reviews',
+        populate:{
+            path:'author'
+        }
+    }).populate('author');//populate reviews array with actual review documents instead of just their ids
+    if(!campground){
+        req.flash('error','Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/show',{ campground });
+};
+
+module.exports.renderEditForm = async(req,res)=>{
+    const campground=await Campground.findById(req.params.id);
+    if(!campground){
+        req.flash('error','Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/edit',{ campground });
+};
+
+module.exports.updateCampground = async(req,res)=>{
+    const { id } = req.params;
+    //  const campground=await Campground.findById(req.params.id);
+    // if(campground.author.equals(req.user._id)===false){//check if the logged in user is the author of the campground
+    //     req.flash('error','You do not have permission to do that!');
+    //     return  res.redirect(`/campgrounds/${id}`);
+    // }
+    const campground=await Campground.findByIdAndUpdate(id, req.body.campground);
+    req.flash('success','Successfully updated campground!');
+    res.redirect(`/campgrounds/${id}`);
+};
+
+module.exports.deleteCampground = async(req,res)=>{
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+};
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,61 +4,21 @@ const Campground=require('../models/campground.js');
 const { isLoggedIn } = require('../middleware.js');
 const { authorize } = require('passport');
 const {validateCampground,isAuthor}=require('../middleware.js');
+const campgrounds=require('../controllers/campgrounds.js');
+const { render } = require('ejs');
 
-router.get('/',async(req,res)=>{
-    const campgrounds=await Campground.find({});
-    res.render('campgrounds/index',{ campgrounds });
-});
-router.get('/new',isLoggedIn,(req,res)=>{
-    // if(!req.isAuthenticated()){//isAuthenticated method is added by passport to req object to check if user is logged in
-    //     req.flash('error','You must be signed in first!');
-    //     return res.redirect('/login');
-    // }//moving this to a middleware
-    res.render('campgrounds/new');
-});
-router.post('/',isLoggedIn,isAuthor,validateCampground,async(req,res)=>{//we are adding isLoggedIn middleware to protect this route from postman attacks
-    const campground=new Campground(req.body.campground);
-    campground.author=req.user._id;//set the author of the campground to the currently logged in user
-    await campground.save();
-    req.flash('success','Successfully made a new campground!');
-    res.redirect(`/campgrounds/${campground._id}`);
-});
-router.get('/:id',async(req,res)=>{
-    const campground=await Campground.findById(req.params.id).populate({//populate the reviews with all their authors
-        path:'reviews',
-        populate:{
-            path:'author'
-        }
-    }).populate('author');//populate reviews array with actual review documents instead of just their ids
-    if(!campground){
-        req.flash('error','Cannot find that campground!');
-        return res.redirect('/campgrounds');
-    }
-    res.render('campgrounds/show',{ campground });
-});
-router.get('/:id/edit',isLoggedIn,isAuthor,async(req,res)=>{
-    const campground=await Campground.findById(req.params.id);
-    if(!campground){
-        req.flash('error','Cannot find that campground!');
-        return res.redirect('/campgrounds');
-    }
-    res.render('campgrounds/edit',{ campground });
-});
-router.put('/:id',isLoggedIn,validateCampground,async(req,res)=>{
-    const { id } = req.params;
-    //  const campground=await Campground.findById(req.params.id);
-    // if(campground.author.equals(req.user._id)===false){//check if the logged in user is the author of the campground
-    //     req.flash('error','You do not have permission to do that!');
-    //     return  res.redirect(`/campgrounds/${id}`);
-    // }
-    const campground=await Campground.findByIdAndUpdate(id, req.body.campground);
-    req.flash('success','Successfully updated campground!');
-    res.redirect(`/campgrounds/${id}`);
-});
-router.delete('/:id',isLoggedIn,async(req,res)=>{
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
-});
+router.get('/',campgrounds.index);
+
+router.get('/new', isLoggedIn, campgrounds.renderNewForm);
+
+router.post('/',isLoggedIn,isAuthor,validateCampground,campgrounds.createCampground);
+
+router.get('/:id',campgrounds.showCampground);
+
+router.get('/:id/edit',isLoggedIn,isAuthor,campgrounds.renderEditForm);
+
+router.put('/:id',isLoggedIn,validateCampground,isAuthor,campgrounds.updateCampground);
+
+router.delete('/:id',isLoggedIn,isAuthor,campgrounds.deleteCampground);
 
 module.exports=router;
